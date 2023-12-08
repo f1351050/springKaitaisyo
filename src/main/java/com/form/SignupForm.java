c02fdf850c327f7a68bd1efa18d2a254b7678f4a
f1351050 <f1351050@gmail.com>
2023-12-08 13:37:45 +0900

    6.2.1

--- src/main/java/com/form/SignupForm.java
+++ src/main/java/com/form/SignupForm.java
@@ -7,15 +7,14 @@ import org.springframework.format.annotation.DateTimeFormat;
 import lombok.Data;
 
 @Data
-public class SingupForm {
+public class SignupForm {
 	private String userId;
 	private String password;
 	private String userName;
 	
-	@DateTimeFormat(pattern="yyyy/MM/dd")
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
 	private Date birthday;
 	
 	private Integer age;
 	private Integer gendar;
-	
 }
