a31f0f233b05c45ca8abaa4f7c39cf57834f425e
f1351050 <f1351050@gmail.com>
2023-12-13 13:51:40 +0900

    9.2.3

--- src/main/java/com/aspect/LogAspect.java
+++ src/main/java/com/aspect/LogAspect.java
@@ -1,7 +1,9 @@
 package com.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -21,5 +23,25 @@ public class LogAspect {
 	@After("execution(* *..*.*UserService.*(..))")
 	public void endLog(JoinPoint jp) {
 		log.info("メソッド修了:"+jp.getSignature());
+	}
+	
+	//@Around("bean(*Controller)")
+	//@Around("@annotation(org.springframework.web.bind.annotation.GetMaping)")
+	@Around("@within(org.springframework.stereotype.Controller)")
+	public Object startLog(ProceedingJoinPoint jp)throws Throwable{
+		log.info("メソッド開始:"+jp.getSignature());
+		
+		try {
+			Object result = jp.proceed();
+			
+			log.info("メソッド終了:"+jp.getSignature());
+			
+			return result;
+			
+		}catch(Exception e) {
+			log.error("メソッド異常修了:"+jp.getSignature());
+			throw e;
+		}
+	
 	}
 }
